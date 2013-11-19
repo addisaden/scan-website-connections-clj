10cdfcfc1588d70b0d6b26ff712533fdef79448d
Adrian E <addis.aden@gmail.com>
2013-11-19 13:49:27 +0100

    create-url works

--- src/website_connections/core.clj
+++ src/website_connections/core.clj
@@ -2,6 +2,8 @@
   (:use seesaw.core)
   (:gen-class))
 
+
+
 (def main-window (frame :title "Website connections"
                         :on-close :exit
                         :minimum-size [640 :by 480]
@@ -9,16 +11,39 @@
 
 (def links-to-scan (atom '()))
 (def links-scanned (atom []))
+; { host {urls [contained-urls]} }
 (def hosts (atom '{}))
+
+(defn get-host-url [u] {:host (.getHost (java.net.URI. u)) :url u})
+
+(defn create-host-if-not-exist [host]
+  (dosync
+    (if (not (some #(= % host) (keys @hosts)))
+      (swap! hosts conj { host {} })
+      )))
+
+(defn create-url [u, contained-links]
+  (let [umap (get-host-url u)]
+    (dosync
+      (create-host-if-not-exist (:host umap))
+      (swap! hosts update-in [(:host umap)] conj {u (vec contained-links)})
+      )))
 
 (defn link-scanned? [u]
   (find @links-scanned u))
 
-(defn get-host-url [u] {:host (.getHost (java.net.URI. u)) :url u})
-
 (defn get-links [u]
   (let [website-source (slurp u)]
-    (re-seq #"(?im)https?:\/\/[^\s\"\']+" website-source)
+    (try
+      (re-seq #"(?im)https?:\/\/[^\s\"\']+" website-source)
+      (catch Exception e (do (println "Error on url:" (.getMessage e))
+                             '()
+                             )))
+    ))
+
+(defn go-test [u]
+  (let [l (get-links u)]
+    (create-url u l)
     ))
 
 (defn -main
